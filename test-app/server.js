8fd47d61f19bb7c7b7a8f30acd7a91342823bd42
ummaraali2 <syeda.umm@northeastern.edu>
2026-08-16 17:26:47 -0400

    change greeting response

--- test-app/server.js
+++ test-app/server.js
@@ -7,7 +7,7 @@ app.get("/health", (req, res) => {
 });
 
 app.get("/greeting", (req, res) => {
-  res.json({ message: "hello", version: 1 });
+  res.json({ message: "hello world", version: 2 });
 });
 
 app.listen(4000, () => console.log("test app on :4000"));

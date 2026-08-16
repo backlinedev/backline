2bae33f8d0f0f0b08bb2427bbb0fbcd5c331b060
ummaraali2 <syeda.umm@northeastern.edu>
2026-08-16 17:25:44 -0400

    add test app and backline config

---
+++ test-app/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.get("/greeting", (req, res) => {
+  res.json({ message: "hello", version: 1 });
+});
+
+app.listen(4000, () => console.log("test app on :4000"));
